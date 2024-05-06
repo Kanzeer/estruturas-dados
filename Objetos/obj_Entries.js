401d3dbce4593a56c7a261ac2f5540b6b67eeb04
Kanzeer <miguelroaamaral@gmail.com>
2024-05-06 16:28:39 -0400

    Adicionado metodo objeto Entries

---
+++ Objetos/obj_Entries.js
@@ -0,0 +1,7 @@
+const person = {
+    name: "Miguel",
+    age: 16,
+    city: "Vg city"
+};
+const entries = Object.entries(person);
+console.log(entries); //exit: [['name', 'Miguel'], ['age', '16'], ['city', 'Vg city']]

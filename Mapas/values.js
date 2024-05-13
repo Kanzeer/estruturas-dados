43d92f8014961b9b28e5cd594c8a8b1240aef635
Kanzeer <miguelroaamaral@gmail.com>
2024-05-13 17:27:48 -0400

    adicionado metodo mapas Values

---
+++ Mapas/values.js
@@ -0,0 +1,13 @@
+let mapa = new Map();
+mapa.set('chave1', 'valor1');
+mapa.set('chave2', 'valor2');
+
+let iterador = mapa.values();
+for (let chave of iterador) {
+    console.log(chave);
+}
+/*
+exit
+valor1
+valor2
+*/
